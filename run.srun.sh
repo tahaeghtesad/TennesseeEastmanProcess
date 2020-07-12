b979eb2cee3bfa94df59b5c42351de5c0d88ef66
Taha Eghtesad <tahaeghtesad@gmail.com>
2020-07-11 19:15:05 -0500

    Update sabine run script

--- run.srun.sh
+++ run.srun.sh
@@ -9,12 +9,12 @@
 
 #SBATCH -A laszka
 
-module load GCC/7.2.0-2.29
-module load Anaconda3/python-3.6
+##module load GCC/7.2.0-2.29
+##module load Anaconda3/python-3.6
 ###module load cuDNN/7.5.0-CUDA-10.0.130
 
 conda activate tep
 cd /project/laszka/TennesseeEastmanProcess/
-export PATH=$PWD/gambit:$PATH
+export PATH=$PWD/gambit-project/:$PATH
 
 python cli.py "$@"
