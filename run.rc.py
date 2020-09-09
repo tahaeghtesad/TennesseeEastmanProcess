f8fa0c026f4b8bdf86c1f07a5cefb4e91f29f08b
Taha Eghtesad <tahaeghtesad@gmail.com>
2020-09-09 03:38:45 -0500

    Fixed a missing comma

--- run.rc.py
+++ run.rc.py
@@ -17,11 +17,11 @@ if __name__ == '__main__':
          '--training_params_tb_logging', 'True',
          '--max_iter', '14',
          '--training_params_observation_history', 'True',
-         '--training_params_limited_history', 'True'
+         '--training_params_limited_history', 'True',
          '--env_params_compromise_actuation_prob', '0.5',
          '--env_params_compromise_observation_prob', '0.5',
          '--env_params_noise', 'True',
-         '--rl_params_random_exploration', '0.2',
+         '--rl_params_random_exploration', '0.1',
          '--rl_params_gamma', '0.9',
          '--policy_params_activation', 'tanh',
          '--policy_params_layers', "64, 64, 64, 64",
