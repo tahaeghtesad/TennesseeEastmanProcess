2db4f41a8ab272d8e31d0bc3f5be6c74711a7ffb
Taha Eghtesad <tahaeghtesad@gmail.com>
2020-12-19 17:49:29 -0600

    Update run config

--- envs/control/envs/BioReactor.py
+++ envs/control/envs/BioReactor.py
@@ -16,8 +16,8 @@ class BioReactor(ControlEnv):
         self.x = np.array([0., 0.])
         self.t_epoch = t_epoch
 
-        self.action_space = gym.spaces.Box(low=np.array([0.0, 0.0]), high=np.array([5.0, 5.0]))
-        self.observation_space = gym.spaces.Box(low=np.array([0.0, 0.0]), high=np.array([10., 10.]))
+        self.action_space = gym.spaces.Box(low=np.array([0.0, 0.0]), high=np.array([3.0, 8.0]))
+        self.observation_space = gym.spaces.Box(low=np.array([0.05, 0.05]), high=np.array([10., 10.]))
 
         # self.observation_space = gym.spaces.Box(low=np.array([0.6, 1] * history_length + [0.] * (self.adversarial_control_env.env.action_dim + self.adversarial_control_env.env.observation_dim)) if self.include_compromise else np.array([-5., -5.] * history_length),
         #                                         high=np.array([1.2, 2.] * history_length + [1.] * (self.adversarial_control_env.env.action_dim + self.adversarial_control_env.env.observation_dim)) if self.include_compromise else np.array([5., 5.] * history_length))
