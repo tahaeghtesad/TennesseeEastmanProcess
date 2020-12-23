0ae5df4acab830c6775667eec248a8e47d7b6a8e
Taha Eghtesad <tahaeghtesad@gmail.com>
2020-12-23 17:58:13 -0600

    Update run config

--- run.rc.full.py
+++ run.rc.full.py
@@ -102,47 +102,52 @@ def generate_runs(repeat, index, parallelization):
 
     # change compromise actuation
     for cu in [0.0, 0.25, 0.5, .75, 1.]:
-        runs.append(create_run(
-            index, 'do_compromise_actuation', compromise_actuation_prob=cu, parallelization=parallelization
-        ))
-        index += 1
-        count += 1
+        for r in range(repeat):
+            runs.append(create_run(
+                index, 'do_compromise_actuation', compromise_actuation_prob=cu, parallelization=parallelization
+            ))
+            index += 1
+            count += 1
 
     # change compromise observation
     for cy in [0.0, 0.25, 0.5, .75, 1.]:
-        runs.append(create_run(
-            index, 'do_compromise_observation', compromise_observation_prob=cy, parallelization=parallelization
-        ))
-        index += 1
-        count += 1
+        for r in range(repeat):
+            runs.append(create_run(
+                index, 'do_compromise_observation', compromise_observation_prob=cy, parallelization=parallelization
+            ))
+            index += 1
+            count += 1
 
     # compromise both
     for cucy in [0., 0.5, 1.]:
-        runs.append(create_run(
-            index,
-            'do_compromise_both',
-            compromise_observation_prob=cucy,
-            compromise_actuation_prob=cucy,
-            parallelization=parallelization
-        ))
-        index += 1
-        count += 1
+        for r in range(repeat):
+            runs.append(create_run(
+                index,
+                'do_compromise_both',
+                compromise_observation_prob=cucy,
+                compromise_actuation_prob=cucy,
+                parallelization=parallelization
+            ))
+            index += 1
+            count += 1
 
     # change attacker power
     for p in [.05, 0.1, 0.2, 0.3, 0.5, 0.75, 1.]:
-        runs.append(create_run(
-            index, 'do_power', power=p, parallelization=parallelization
-        ))
-        index += 1
-        count += 1
+        for r in range(repeat):
+            runs.append(create_run(
+                index, 'do_power', power=p, parallelization=parallelization
+            ))
+            index += 1
+            count += 1
 
     # change include compromise
     for ic in [True, False]:
-        runs.append(create_run(
-            index, 'do_compromise', include_compromise=ic, parallelization=parallelization
-        ))
-        index += 1
-        count += 1
+        for r in range(repeat):
+            runs.append(create_run(
+                index, 'do_compromise', include_compromise=ic, parallelization=parallelization
+            ))
+            index += 1
+            count += 1
 
     print(f'Total {count} jobs were created.')
     return runs
@@ -177,9 +182,9 @@ python run.rc.py $SLURM_ARRAY_TASK_ID
 
 if __name__ == '__main__':
     parallelization = 2
-    start_index = 13000
-    concurrent_runs = 350
-    repeat = 10
+    start_index = 15000
+    concurrent_runs = 50
+    repeat = 4
     runs = generate_runs(repeat, start_index, parallelization)
     assert len(runs) < 1001, 'Too many runs to schedule'
     if len(sys.argv) == 1:
