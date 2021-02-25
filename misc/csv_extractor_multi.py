4db790746450ef146e5dc30a21e341ac716c2a40
Taha Eghtesad <tahaeghtesad@gmail.com>
2021-02-25 13:36:41 -0600

    Update extractors

--- misc/csv_extractor_multi.py
+++ misc/csv_extractor_multi.py
@@ -2,25 +2,24 @@ import csv
 import numpy as np
 from lazy_streams import stream
 
-path = '/Users/taha/PycharmProjects/TennesseeEastmanProcess/misc/wandb_export_2021-02-22T14_02_36.805-06_00.csv'
-env = 'TT'
+path = '/Users/taha/PycharmProjects/TennesseeEastmanProcess/misc/wandb_export_2021-02-24T14_16_06.088-06_00.csv'
 
-groups = {'layers': 'policy_params.layers',
-          'batch_size': 'rl_params.batch_size',
-          't_epoch': 'env_params.t_epoch',
-          'act_fun': 'policy_params.act_fun',
-          'gamma': 'rl_params.gamma',
-          'buffer_size': 'rl_params.buffer_size',
-          'history_length': 'env_params.history_length',
-          'action_noise': 'training_params.action_noise_sigma',
-          'env_noise': 'env_params.noise_sigma',
-          'baseline': None,
-          'epsilon': 'rl_params.random_exploration',
-          'start_set': None
-      }
+groups = {'do_baseline': None,
+          'do_observation_only': 'env_params.compromise_observation_prob',
+          'do_memory': 'env_params.history_length',
+          'do_power': 'env_params.power',
+          'do_start_set': 'env_params.test_env',
+          'do_env_noise': 'env_params.noise_sigma',
+          'do_actuation_only': 'env_params.compromise_actuation_prob',
+          'do_baseline_random_start': None
+          }
+
+print(groups.keys())
+
+env = 'BRP'
 
 with open(path) as fd:
-    reader = csv.DictReader(fd, quotechar='"', delimiter=",")
+    reader = csv.DictReader(fd)
     rows = list(reader)
 
 variables = dict()
@@ -34,8 +33,8 @@ for row in rows:
     else:
         variables[v] = {row[v]}
 
-payoff = dict()
-times = dict()
+improvements = dict()
+
 for g in groups:
     for v in variables[groups[g]] if groups[g] is not None else [None]:
         key = (g, v)
@@ -44,22 +43,19 @@ for g in groups:
             if row['Group'] == g:
                 if row['env_id'] == env:
                     if groups[g] is None or row[groups[g]] == v:
-                        p = row['base_defender_payoff']
-                        t = row['Runtime']
-                        if p == '':
-                            continue
-                        p = float(p)
-                        t = int(t)
-                        if key in payoff:
-                            payoff[key].append(p)
-                        else:
-                            payoff[key] = [p]
 
-                        if key in times:
-                            times[key].append(t)
-                        else:
-                            times[key] = [t]
+                        msne_br = float(row['final_payoff/msne_br'])
+                        base_br = float(row['final_payoff/base_br'])
+                        no_def = float(row['final_payoff/no_defense'])
+                        msne_table = float(row['final_payoff/msne_table'])
+                        msne_eval = float(row['final_payoff/msne_eval'])
 
+                        improvement = (msne_br - base_br) / base_br * 100.
+
+                        if key in improvements:
+                            improvements[key].append(improvement)
+                        else:
+                            improvements[key] = [improvement]
 
 def removeOutliers(x, outlierConstant=1.5):
     a = np.array(x)
@@ -73,11 +69,10 @@ def removeOutliers(x, outlierConstant=1.5):
             resultList.append(y)
     return resultList
 
-
-p_percentiles = dict()
-for k in payoff.keys():
-    arr = np.array(payoff[k])
-    p_percentiles[k] = {
+improvement_percentiles = dict()
+for k in improvements.keys():
+    arr = np.array(improvements[k])
+    improvement_percentiles[k] = {
         'min': np.min(removeOutliers(arr)),
         'first': np.quantile(arr, 0.25),
         'median': np.quantile(arr, 0.5),
@@ -91,10 +86,9 @@ def type_aware_sort(arr):
     except:
         return sorted(arr)
 
-
 for g in groups:
 
-    with open(f'single-agent-avg/{g}_p_{env}_avg.csv', 'w+') as fd:
+    with open(f'multi-agent/{g}_msne_{env}.csv', 'w+') as fd:
         writer = csv.writer(fd, quoting=csv.QUOTE_NONE, escapechar=' ')
         #median box_top box_bottom whisker_top whisker_bottom name
         # writer.writerow(['index', 'name', 'whisker_bottom', 'box_bottom', 'median', 'box_top', 'whisker_top'])
@@ -102,7 +96,7 @@ for g in groups:
         index = 1
         for v in type_aware_sort(list(variables[groups[g]])) if groups[g] is not None else [None]:
             key = (g, v)
-            writer.writerow([index, p_percentiles[key]['median'], p_percentiles[key]['first'], p_percentiles[key]['third'], p_percentiles[key]['max'], p_percentiles[key]['min'], v.replace(',', '-').replace(' ', '') if v is not None else g.replace('_', '-')])
+            writer.writerow([index, improvement_percentiles[key]['median'], improvement_percentiles[key]['first'], improvement_percentiles[key]['third'], improvement_percentiles[key]['max'], improvement_percentiles[key]['min'], v.replace(',', '-').replace(' ', '') if v is not None else g.replace('_', '-')])
             index += 1
 
 for g in groups:
